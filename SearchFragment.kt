2bbfee9eb5a68e18c4947cd3ca68fcc6d5413108
Note <85052819+4086@users.noreply.github.com>
2022-11-15 01:32:21 +0900

    Update and rename SearchFragment to SearchFragment.kt

--- SearchFragment.kt
+++ SearchFragment.kt
@@ -1,11 +1,13 @@
 package com.example.sns_project
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -54,6 +56,10 @@ class SearchFragment : Fragment() {
 
         val searchBar = view.findViewById<EditText>(R.id.searchuser)
         val searchbtn = view.findViewById<Button>(R.id.searchbtn)
+
+        searchBar.requestFocus()
+        val imm = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
 
         searchbtn.setOnClickListener {
             val uid = searchBar.text.toString()
